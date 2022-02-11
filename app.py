1cad3f2f4da2f1a551356309fe7d4abd44fb9d24
j <ajuberiya@gmail.com>
2022-02-12 00:32:04 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 from flask_sqlalchemy import SQLAlchemy
  
 
@@ -10,17 +9,7 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
-
-    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
