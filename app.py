de02c3c51ed6524aa5871986f071291f763c1e2c
j <ajuberiya@gmail.com>
2022-02-13 00:33:42 +0530

    added del account.html

--- app.py
+++ app.py
@@ -214,6 +214,34 @@ def register():
     else:
         return render_template("register.html")
 
+@app.route("/delete" , methods=["GET", "POST"])
+@login_required
+def delete():
+    if request.method == "POST":
+        user_id=session["user_id"]
+       # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        db.execute('DELETE FROM users WHERE id = ?', (user_id))
+        session.clear()
+
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("del_account.html")
+
+
 
 
 
